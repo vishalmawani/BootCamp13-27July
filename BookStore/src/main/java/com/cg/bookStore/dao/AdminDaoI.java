13eecb88c8035f22369cd241a166a3eb94b0aa9a
AshokSharma85 <ashoksharma8504@gmail.com>
2020-07-19 16:36:59 +0530

    Main Application

--- BookStore/src/main/java/com/cg/bookStore/dao/AdminDaoI.java
+++
@@ -1,8 +0,0 @@
-package com.cg.bookStore.dao;
-
-import com.cg.bookStore.entities.Admin;
-
-public interface AdminDaoI {
-
-	public void addAdmin(Admin admin);
-}
--- BookStore/src/main/java/com/cg/bookStore/service/AdminServiceI.java
+++
@@ -1,9 +0,0 @@
-package com.cg.bookStore.service;
-
-import com.cg.bookStore.entities.Admin;
-import com.cg.bookStore.exceptions.AdminServiceException;
-
-public interface AdminServiceI {
-
-	public String addAdmin(Admin admin) throws AdminServiceException;
-}
